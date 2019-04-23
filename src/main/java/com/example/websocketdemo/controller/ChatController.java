f0d933087533bd36761ff2a8d3e99359552aaefc
Safanar <satan251008@gmail.com>
2019-04-23 14:06:00 +0800

    190423

--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -1,38 +1,24 @@
 package com.example.websocketdemo.controller;
 
 import com.example.websocketdemo.model.ChatMessage;
-import com.example.websocketdemo.controller.WebSocketEventListener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import com.example.websocketdemo.util.JsonUtil;
 
 @Controller
 public class ChatController {
@@ -59,7 +45,7 @@ public class ChatController {
             String path = request.getServletContext().getRealPath(predix) + imageName;
             File localImageFile = new File(path);
             try {
-                imageFile.transferTo(localImageFile); //文件转存到服务器文件中
+                imageFile.transferTo(localImageFile);
                 ChatMessage chatMessage = new ChatMessage();
                 chatMessage.setType(ChatMessage.MessageType.PIC);
                 chatMessage.setSender(userName);
@@ -67,7 +53,7 @@ public class ChatController {
                 messagingTemplate.convertAndSend("/topic/public", chatMessage);
                 return path;
             } catch (IOException e) {
-                logger.error("图片上传失败：" + e.getMessage());
+                logger.error("upload false：" + e.getMessage());
                 return "upload false";
             }
         }
